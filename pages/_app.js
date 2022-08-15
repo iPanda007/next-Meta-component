1eba64143bbd83b9c30fec1be0ed4d2acc503827
iPanda <aungkyawkhaing2000@gmail.com>
2022-08-16 01:15:39 +0630

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
-
+import Layout from '../components/Layout'
+import Nav from '../components/Nav'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <Nav/>
+    <Layout>
+     <Component {...pageProps} />
+  </Layout>
+    </>
+  )
 }
 
 export default MyApp
